0a07f2ea14f4d5c9cbd296dd2d0ef26efb505ae9
Tyler Guillen <tyguillen@gmail.com>
2017-11-30 23:16:07 -0800

    stuff

---
+++ lib/hphones/version.rb
@@ -0,0 +1,3 @@
+module Hphones
+  VERSION = "0.0.1"
+end
---
+++ spec/ruby_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe Hphones do
+  it "has a version number" do
+    expect(Hphones::VERSION).not_to be nil
+  end
+end
